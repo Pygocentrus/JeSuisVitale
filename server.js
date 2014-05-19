7e453303aa0aa949b1998b6e558c426b682e4325
Pygocentrus <pierre.guilhou14@gmail.com>
2014-05-19 19:36:18 +0200

    INIT Adding scrapping algorithm and data files

---
+++ server.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var fs = require('fs');
+var http = require('http');
+var Q = require('q');
+var app = express();
+
+// FULL PATH : path: '/get-reseau-eau.php?zipCodeOrCity=18000&radius=100&commune_id='
+
+app.get('/', function(req, res) {
+	//Getting all the french zip codes
+	fs.readFile('villes.json', 'utf-8', function(err, data) {
+		if (!err) {
+			var villes = JSON.parse(data);
+			var zipCode, options, str = '', promises = new Array();
+
+			console.log(villes.length+' cities to check');
+			console.log('START Grabbing data...');
+
+			var promises = Q();
+			//Searching data for each corresponding city
+			for (var i = 0, size = villes.length; i < size; i++) {
+				promises = promises.then(grabRow(villes[i], i));
+			}
+			promises.then(function(status){
+				console.log('All done !');
+			});
+		} else {
+			console.log('Err opening villes.json');
+		}
+	});
+	res.send('Work in progress...');
+});
+
+//Search for a city, then writes it to the appropriate file (eau.json)
+function grabRow(ville, index){
+	return function(status){
+		var zipCode = ville.Codepos;
+		var str = '';
+		var waterData = '';
+		var options = {
+			host: 'maps.quechoisir.org',
+			path: '/get-reseau-eau.php?zipCodeOrCity='+zipCode+'&radius=1'
+		};
+		var deferred = Q.defer();
+
+		console.log('-City #'+(index+1)+': ' + ville.Commune + ' (' + zipCode + ')');
+
+		//Launches the request
+		http.request(options, function(response) {
+			response.on('data', function dataReceived(chunk) {
+				str += chunk;
+			});
+			response.on('end', function requestEnded() {
+				var received = JSON.parse(str);
+				if(received.data){
+					console.log('  --> Grabbing data');
+					waterData = fs.readFileSync('eau.json', 'utf-8');
+					waterData = JSON.parse(waterData);
+					waterData.push(received);
+					//Writes back the final file's content according to the previous results
+					fs.writeFileSync('eau.json', JSON.stringify(waterData), 'utf-8');
+					console.log('  --> Data saved');
+				}
+				deferred.resolve(200);
+			});
+		}).end();
+		return deferred.promise;
+	}
+}
+
+//Launches the server
+var server = app.listen(3000, function() {
+	console.log('Listening on port %d', server.address().port);
+});

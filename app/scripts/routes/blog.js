a66d1b45e6a90fad1dd923480613a5c1c803147b
Pygocentrus <pierre.guilhou14@gmail.com>
2014-05-24 17:46:43 +0200

    INIT front - backbone and requireJS scaffolding

---
+++ app/scripts/routes/blog.js
@@ -0,0 +1,16 @@
+/*global define*/
+
+define([
+    'jquery',
+    'backbone'
+], function ($, Backbone) {
+    'use strict';
+
+    var BlogRouter = Backbone.Router.extend({
+        routes: {
+        }
+
+    });
+
+    return BlogRouter;
+});

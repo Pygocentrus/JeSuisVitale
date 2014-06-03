0fe3d2c56b93dfbee7b888b8778c228855bc5ee8
Pierre Guilhou <pierre.guilhou14@gmail.com>
2014-06-03 11:51:30 +0200

    UPDATE Adding d3.js in RequireJS configuration

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,7 +19,7 @@ require.config({
     },
     paths: {
         jquery: '../bower_components/jquery/dist/jquery',
-        backbone: '../bower_components/d3/d3.js',
+        d3: '../bower_components/d3/d3.js',
         backbone: '../bower_components/backbone/backbone',
         underscore: '../bower_components/underscore/underscore',
         handlebars: '../bower_components/handlebars/handlebars',

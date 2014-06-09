a20cdaf6efcc6f104bfb4b96953356396d2e98d3
Kévin <kevinchassagne@gmail.com>
2014-06-09 11:01:48 +0200

    Add bg + fonts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -33,26 +33,26 @@ require([
     Backbone.history.start();
     
     // MAP 
-    (function() {
+    // (function() {
 
-        var path = document.querySelectorAll('path'); 
-        var departements = new Array();
+    //     var path = document.querySelectorAll('path'); 
+    //     var departements = new Array();
 
-        for (var i = 0; i < 94; i++) {
-            departements.push(path[i]);
-        };
+    //     for (var i = 0; i < 94; i++) {
+    //         departements.push(path[i]);
+    //     };
 
-        for (var i = 0, size = departements.length; i < size; i++) {
-            departements[i].addEventListener('mouseover', function() {
-                this.style.fill = 'red';
-            }, false);
-            departements[i].addEventListener('mouseout', function() {
-                this.style.fill = '#EEEEEE';
-            }, false);
-        }; 
+    //     for (var i = 0, size = departements.length; i < size; i++) {
+    //         departements[i].addEventListener('mouseover', function() {
+    //             this.style.fill = '#5095F9';
+    //         }, false);
+    //         departements[i].addEventListener('mouseout', function() {
+    //             this.style.fill = '#EEEEEE';
+    //         }, false);
+    //     }; 
 
-        console.log(departements);
-    }).call(this);
+    //     console.log(departements);
+    // }).call(this);
 
     // BLUR
     (function() {

b9cabacb797c3e4699f28286d4d3459d97dd79ae
Pygocentrus <pierre.guilhou14@gmail.com>
2014-06-12 18:53:21 +0200

    ADDING City changing feature

--- app/scripts/custom/_vars.js
+++ app/scripts/custom/_vars.js
@@ -1,5 +1,7 @@
 /* Instanciates every UI element vars we need for the app */
 var h2           = document.querySelector('.menu h2'),
+    lvl1         = document.querySelector('#level-1'),
+    lvl2         = document.querySelector('#level-1'),
     list         = document.querySelectorAll('.menu ul li'),
     href         = document.querySelectorAll('.menu a'),
     goDown       = document.querySelector('.go-down'),
@@ -7,5 +9,6 @@ var h2           = document.querySelector('.menu h2'),
     section      = document.querySelectorAll('.section-height'),
     mapWrapper   = document.querySelector('#map-wrapper'),
     pGeolocation = document.querySelector('.geolocation');
+
 
 
